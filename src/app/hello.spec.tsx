c2f08fd41ec307d95393f4cce0f4d6db19423fc0
Joshua.Toth <tothjoshuaj@gmail.com>
2017-06-08 17:49:06 +1000

    Initial framework. React, typescript, gulp

---
+++ src/app/hello.spec.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import * as TestUtils from 'react-addons-test-utils';
+import { Hello } from './hello';
+
+describe('hello component', function() {
+});
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import {Hello} from './app/hello';
+
+import './index.less';
+
+ReactDOM.render(
+  <Hello/>,
+  document.getElementById('root')
+);
